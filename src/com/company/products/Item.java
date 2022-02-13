fea28ab1e1e930169b0908ae034d147b8cc6fc5b
Vilkro <thekodyz@gmail.com>
2022-02-13 20:25:14 +0600

    im working on my project.

---
+++ src/com/company/products/Item.java
@@ -0,0 +1,59 @@
+package com.company.products;
+
+public class Item {
+    private int id;
+    private String name;
+    private double price;
+    private int rating;
+
+    public Item(){
+
+    }
+    public Item(int id, String name, double price, int rating){
+        setId(id);
+        setName(name);
+        setPrice(price);
+        setRating(rating);
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", rating=" + rating +
+                '}';
+    }
+}
