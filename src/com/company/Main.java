36b373dcd18f76f2af3b609c040ba96756dcf53f
Vilkro <thekodyz@gmail.com>
2022-02-22 01:59:41 +0600

    filled that UI with all the func

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import com.company.repositories.interfaces.prlist;
+import com.company.repositories.repository;
+import com.company.data.interfaces.IDB;
+import com.company.data.postresql;
+import com.company.products.Item;
+import com.company.Product;
+import com.company.cart;
+import com.company.ui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IDB db = new postresql();
+        prlist repo = new repository(db);
+        cart controller = new cart(repo);
+        ui userinterface = new ui(controller);
+        userinterface.launch();
+    }
+}
